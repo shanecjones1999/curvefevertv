c0ffb5f74acc7b9e5b2f8140100068d37c641a5b
Shane Jones <shane@Shanes-MacBook-Pro.local>
2026-03-13 08:14:00 -0400

    Fix backend URL

--- curvefever-frontend/src/socket.ts
+++ curvefever-frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const BACKEND_URL = "http://localhost:3001";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 console.log("Connecting to backend at", BACKEND_URL);
 export const socket: Socket = io(BACKEND_URL, {
     autoConnect: true,
